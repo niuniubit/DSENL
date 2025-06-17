b86ea24e1a4e92a808210c9bdcaffaea24172c09
LiuWH <67543565+niuniubit@users.noreply.github.com>
2025-06-17 11:22:10 +0800

    Update main_DSENL.m

--- main_DSENL.m
+++ main_DSENL.m
@@ -23,28 +23,14 @@ anchorIndex = sort(randperm(n, n_anchors));
 [Ktr1, Ktr2, Kte1, Kte2, Kanchor1, Kanchor2] = kernelTrans(I_tr, T_tr, I_te, T_te, anchorIndex);
 %% initial data
 param.nbits = 16;
-if strcmp(db_name, 'NUSWIDE')
-    param.beta = 1e3;
-    param.mu = 1e0;
-    param.phi = 1e-1;
-    param.eta = 1e1;
-    param.lambda = 1e-3;
-    param.alpha = 1e-4;
-elseif strcmp(db_name, 'MIRFLICKR')
-    param.beta = 1e3;
-    param.mu = 1e-1;
-    param.phi = 1e-2;
-    param.eta = 1e-2;
-    param.lambda = 1e-3;
-    param.alpha = 1e-1;
-elseif strcmp(db_name, 'IAPRTC-12')
-    param.beta = 1e3;
-    param.mu = 1e-1;
-    param.phi = 1e-2;
-    param.eta = 1e0;
+
+    param.beta = 1e2;
+    param.mu = 1e-2;
+    param.phi = 1e1;
+    param.eta = 1e2;
     param.lambda = 1e-3;
     param.alpha = 1e-2;
-end
+
 param.iter = 10;
 %% train
 eva_info=evaluate_DSENL(Ktr1, Ktr2,LTrain,Kte1, Kte2,LTest,param);
